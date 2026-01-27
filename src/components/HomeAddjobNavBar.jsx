70ff926280b5f514ff3a52321bd0036d11e6b06a
Amos Asiedu <asieduamos6@gmail.com>
2026-01-27 20:41:29 +0000

    py value of navabr buttons changed

--- src/components/HomeAddjobNavBar.jsx
+++ src/components/HomeAddjobNavBar.jsx
@@ -6,19 +6,23 @@ const HomeAddjobNavBar = () => {
   return (
     <>
     <div className="flex h-20 items-center border-b border-indigo-800 ">
-            <div className='pointer-events-none select-none ml-70'><img className='w-20' src={FJ} alt="" /></div> 
+          <div className='flex'>
+            <div className='pointer-events-none select-none ml-60'><img className='w-16' src={FJ} alt="" /></div>
+            <div className='font-extrabold italic text-xl ml-1 mt-3 text-indigo-800'>Find Job</div> 
+          </div>
+            
              
-             <NavLink to="/FindAdd" className='bg-black  text-white font-bold text-xl ml-140 px-5 py-2 text-center rounded-md
+             <NavLink to="/FindAdd" className='bg-black  text-white font-bold text-xl ml-135 px-5 py-1 text-center rounded-md
                               cursor-pointer hover:bg-indigo-900 hover:text-white transition duration-300 ease-in-out'>
                                 Home
                               </NavLink>
              
-             <NavLink to="/findadd" className='bg-white border border-indigo-300 text-indigo-900 font-bold text-xl px-3 py-2 ml-5 mr-5 text-center rounded-md
+             <NavLink to="/findadd" className='bg-white border border-indigo-300 text-indigo-900 font-bold text-xl px-3 py-1 ml-5 mr-5 text-center rounded-md
                               cursor-pointer hover:bg-indigo-900 hover:text-white transition duration-300 ease-in-out'>
                                 Add Job
                               </NavLink>
            
-             <NavLink to="/jobpage" className='bg-white border border-indigo-300 text-indigo-900 font-bold text-xl px-6 py-2  text-center rounded-md
+             <NavLink to="/jobpage" className='bg-white border border-indigo-300 text-indigo-900 font-bold text-xl px-6 py-1  text-center rounded-md
                               cursor-pointer hover:bg-indigo-900 hover:text-white transition duration-300 ease-in-out'>
                   Jobs
              </NavLink>
