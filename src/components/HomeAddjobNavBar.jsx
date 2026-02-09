5243a752d52dae31f3e2bb226e49d978ada2a9df
Amos Asiedu <asieduamos6@gmail.com>
2026-02-09 22:15:50 +0000

    navbar component maked responsive

--- src/components/HomeAddjobNavBar.jsx
+++ src/components/HomeAddjobNavBar.jsx
@@ -5,25 +5,29 @@ import { NavLink } from 'react-router-dom'
 const HomeAddjobNavBar = () => {
   return (
     <>
-    <div className="flex h-20 items-center border-b border-indigo-900 shadow-lg">
+    <div className="flex lg:h-20 md:h-50 h-60 lg:w-full w-6xl items-center border-t border-b border-indigo-600 shadow-lg">
           <div className='flex'>
-            <div className='pointer-events-none select-none ml-60'><img className='w-16' src={FJ} alt="" /></div>
-            <div className='font-extrabold italic text-xl ml-1 mt-3 text-indigo-800'>Find Job</div> 
+            <img className='pointer-events-none select-none lg:ml-30 md:ml-10 ml-15 lg:w-16 md:w-27 w-30' src={FJ} alt="" />
+            <div className='font-extrabold italic lg:text-xl text-5xl lg:ml-1 ml-3 lg:mt-3 md:mt-6 mt-8 text-indigo-800'>
+              Find Job</div> 
           </div>
             
              
-             <NavLink to="/Home" className='bg-black  text-white font-bold text-xl ml-135 px-5 py-1 text-center rounded-md
-                              cursor-pointer hover:bg-indigo-900 hover:text-white transition duration-300 ease-in-out'>
+             <NavLink to="/Home" className='bg-black  text-white lg:font-bold font-light lg:text-xl text-5xl lg:ml-100 md:ml-40 ml-30
+                       px-5 lg:py-1 py-3 text-center rounded-md cursor-pointer hover:bg-indigo-900 hover:text-white
+                       transition duration-300 ease-in-out'>
                                 Home
                               </NavLink>
              
-             <NavLink to="/addjob" className='bg-white border border-indigo-300 text-indigo-900 font-bold text-xl px-3 py-1 ml-5 mr-5 text-center rounded-md
-                              cursor-pointer hover:bg-indigo-900 hover:text-white transition duration-300 ease-in-out'>
+             <NavLink to="/addjob" className='bg-white border border-indigo-300 text-indigo-900 font-bold lg:text-xl text-5xl px-3
+                        lg:py-1 py-3 ml-5 mr-5 text-center rounded-md cursor-pointer hover:bg-indigo-900 hover:text-white 
+                        transition duration-300 ease-in-out'>
                                 Add Job
                               </NavLink>
            
-             <NavLink to="/jobpage" className='bg-white border border-indigo-300 text-indigo-900 font-bold text-xl px-6 py-1  text-center rounded-md
-                              cursor-pointer hover:bg-indigo-900 hover:text-white transition duration-300 ease-in-out'>
+             <NavLink to="/jobpage" className='bg-white border border-indigo-300 text-indigo-900 font-bold lg:text-xl text-5xl
+              px-6 lg:py-1 py-3 text-center rounded-md cursor-pointer hover:bg-indigo-900 hover:text-white 
+              transition duration-300 ease-in-out'>
                   Jobs
              </NavLink>
             
--- src/pages/Head.jsx
+++ src/pages/Head.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Head = () => {
   return (
     <>
-    <div className='lg:text-2xl md:text-5xl text-7xl lg:font-thin md:font-light font-light bg-indigo-500 text-white lg:py-8 py-15 text-center mt-6 lg:w-full w-6xl px-8'>
-        <p className=' '> Your Gateway to Exciting Job Opportunities And Aslo,The perfect Platform  to find the perfect employee by listing 
-              the job vacancies in your company or organization.</p>
+    <div className='lg:text-2xl md:text-5xl text-7xl lg:font-thin md:font-light font-light 
+               bg-indigo-500 text-white lg:py-8 py-15 text-center lg:mt-6 md:mt-12 mt-18 lg:w-full w-6xl px-8'>
+        <p className=' '> Your Gateway to Exciting Job Opportunities And Aslo,The perfect Platform 
+           to find the perfect employee by listing the job vacancies in your company or organization.</p>
 
     </div>
     </>
