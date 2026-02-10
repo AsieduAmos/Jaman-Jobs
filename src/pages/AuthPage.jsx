6cf303b3b39102cc470e194f38891e98d8ceca3c
Amos Asiedu <asieduamos6@gmail.com>
2026-02-10 22:04:05 +0000

    homepage cards component maked responsive

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -8,11 +8,11 @@ const AuthPage = () => {
   <> 
     
     <div className='grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 items-center justify-center
-           pointer-events-none select-none mt-15 mx-30'>
-        <img className='w-120 md:w-140 lg:w-100 lg:ml-50 md:ml-40 ml-10' src={findjob} 
+           pointer-events-none select-none lg:mt-10 mt-15 mx-30'>
+        <img className='lg:w-100 w-200 lg:ml-50 md:ml-40 ml-10' src={findjob} 
           alt=" black young man looking on mobile phone" />
-        <div className='py-10 px-5'>
-          <h2 className='text-3xl text-indigo-900 font-bold mb-8'>
+        <div className='py-1 px-5'>
+          <h2 className='text-3xl  text-indigo-900 font-bold mb-5'>
               Stay Home And <br />Find Job
            </h2>
             <p className='text-xl font-bold text-indigo-800'>Find Job With No Stress With Just A <br />Click
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,28 +10,28 @@ const HomePage = () => {
   <>
    <Head />
    <AuthPage />
-  <div className='flex flex-col md:flex-row items-center justify-center my-15 gap-10 '>
-    <div className='bg-gray-200 text-indigo-900 w-120 p-5 shadow-md rounded-lg'>
-        <h2 className='font-bold text-xl py-3 '>For Employers</h2>
-        <p className='font-normal text-md pb-5'> 
+  <div className='flex lg:flex-row flex-col items-center justify-center lg:mx-10 lg:my-4 my-15 lg:gap-6 gap-10  '>
+    <div className='bg-gray-200 text-indigo-900 lg:w-3xl md:w-5xl w-5xl lg:py-2 py-20 lg:px-3 px-15 lg:ml-1 md:ml-90 ml-180 lg:mt-10 shadow-md lg:rounded-lg rounded-4xl'>
+        <h2 className='lg:font-bold font-semibold lg:text-3xl md:text-5xl text-7xl py-2 '>For Employers</h2>
+        <p className='lg:font-normal font-light lg:text-lg md:text-4xl text-6xl lg:pb-2 pb-15 lg:my-2 my-6'> 
           Our platform makes it easy to find the perfect fit for your team.
           List available job vacancies to find a perfect employee.
           
         </p>
-        <NavLink to="/HomePage" className='bg-indigo-700 text-white font-medium text-xl px-3 py-1 text-center rounded-xl
-                              cursor-pointer hover:bg-black hover:text-white 
-                              transition duration-300 ease-in-out'>Add Job</NavLink>
+        <NavLink to="/HomePage" className='bg-indigo-700 text-white lg:font-normal font-medium lg:text-2xl md:text-4xl text-6xl 
+           lg:px-4 px-15 lg:py-1 md:py-4 py-5 text-center rounded-xl cursor-pointer hover:bg-black
+           hover:text-white transition duration-300 ease-in-out'>Add Job</NavLink>
       </div>
 
-     <div className='bg-gray-100 text-indigo-900 w-120 p-5 shadow-md rounded-lg'>
-        <h2 className='font-bold text-xl py-3 '>For Job Seekers</h2>
-        <p className='font-normal text-md pb-5'> 
+     <div className='bg-gray-100 text-indigo-900 lg:w-3xl md:w-5xl w-5xl lg:py-2 py-20 lg:px-3 px-15 lg:ml-1 md:ml-90 ml-180 mt-10 shadow-md lg:rounded-lg rounded-4xl'>
+        <h2 className='lg:font-bold font-semibold lg:text-3xl md:text-5xl text-7xl py-2 '>For Job Seekers</h2>
+        <p className='lg:font-normal font-light lg:text-lg md:text-4xl text-6xl lg:pb-2 pb-15 lg:my-2 my-6'> 
             Looking for your next career opportunity?
           Find jobs that best fit your skills and aspirations.
         </p>
-        <NavLink to="/HomePage" className='bg-black text-white font-medium text-xl px-3 py-1 text-center rounded-xl
-                              cursor-pointer hover:bg-indigo-700 hover:text-white 
-                              transition duration-300 ease-in-out'>Find Job</NavLink>
+        <NavLink to="/HomePage" className='bg-black text-white lg:font-normal font-medium lg:text-2xl md:text-4xl text-6xl 
+        lg:px-4 px-15 lg:py-1 md:py-4 py-5 text-center rounded-xl cursor-pointer hover:bg-indigo-700 
+        hover:text-white transition duration-300 ease-in-out'>Find Job</NavLink>
       </div>
      </div> 
      <Foot />
