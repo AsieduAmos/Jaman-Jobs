cbdd0422cfff871acc872cd1474b5a20f133c3eb
Amos Asiedu <asieduamos6@gmail.com>
2026-02-11 19:36:28 +0000

    foot component maked responsive

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -5,12 +5,12 @@ const Foot = () => {
   return (
     <> 
     
-    <footer className='bg-indigo-500 text-white py-10 mt-10'>
-        <div className='flex items-center justify-center mt-15 mb-15 pointer-events-none select-none'>
-       <img className='w-18'src={FJ} alt="" />
+    <footer className='bg-indigo-500 text-white py-10 lg:mt-15 mt-30 lg:w-full w-6xl'>
+        <div className='flex items-center justify-center lg:my-5 my-15 pointer-events-none select-none'>
+       <img className='lg:w-18 w-30'src={FJ} alt="" />
     </div>
-        <div className='container mx-auto px-4'>
-            <p className='text-center'>© 2024 FindJobs. All rights reserved.</p>
+        <div className='container mx-auto lg:px-2 '>
+            <p className='text-center lg:text-sm text-5xl'>© 2024 FindJobs. All rights reserved.</p>
         </div>
     </footer>
     </>
