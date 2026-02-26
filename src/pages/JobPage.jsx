3bf690a4506c19d2273442c36db8f75cd28ed903
Amos Asiedu <asieduamos6@gmail.com>
2026-02-26 15:17:43 +0000

    grid gap changed in jobpage

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -20,7 +20,7 @@ const JobPage = () => {
    <>
     
     <div className='bg-slate-100 font-bold text-3xl text-center text-indigo-800 p-4 mx-50 rounded-md mt-5'>Browse Jobs</div>
-     <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
       {jobs.map((job) => (
      <div className='w-90 px-5 py-5 rounded-md shadow-lg mt-10 ml-5'>
         <h2 className='text-2xl font-bold mb-5'>{job.title}</h2>
