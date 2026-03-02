bc3b4d4f813f28b256c841a9e9b32c418e766dd0
Amos Asiedu <asieduamos6@gmail.com>
2026-03-02 12:08:54 +0000

    jobpage restyled

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -19,23 +19,27 @@ const JobPage = () => {
   return (
    <>
     
-    <div className='bg-slate-100 font-bold text-3xl text-center text-indigo-800 p-4 mx-50 rounded-md mt-5'>Browse Jobs</div>
-     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+    <div className=' bg-slate-100 px-8 '>
+      <h2 className='font-bold text-3xl text-center text-indigo-900 p-10 '>Browse Jobs</h2>
+     <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
       {jobs.map((job) => (
-     <div className='w-90 px-5 py-5 rounded-md shadow-lg mt-10 ml-5'>
-        <h2 className='text-2xl font-bold mb-5'>{job.title}</h2>
-        <h4 className='font-thin mb-3'>{job.type}</h4>
-        <p className='mb-3 font-medium'>{job.description}</p>
-      <p className='font-thin text-indigo-500 mb-4'>{job.salary}</p>
+     <div className=' px-5 pt-5 rounded-md shadow-lg bg-white'>
+        <h2 className='text-2xl text-gray-800 font-bold mb-5'>{job.title}</h2>
+        <h4 className='font-thin  text-gray-500 mb-3'>{job.type}</h4>
+        <p className='mb-3 text-gray-700 font-semibold'>{job.description}</p>
+      <p className='font-thin text-gray-500 mb-4'>{job.salary}</p>
 
-        <div className='shadow-2xl py-3'>
-          <h1 className='text-indigo-700 mb-4 ml-4 '>{job.location}</h1>
-          <button className='bg-indigo-600 text-white px-30 rounded-md hover:bg-indigo-800 mx-4 py-2'>Apply</button>
+        <div className='shadow-2xl py-5 px-4 rounded-lg mb-5'>
+          <h1 className='text-red-600 mb-3 ml-2 '>{job.location}</h1>
+          <button className='text-white px-30 py-3 py-2 mx-6 rounded-md bg-indigo-900 hover:bg-gray-900 
+           text-md font-semibold '>
+            Apply</button>
         </div>
      </div>
 
       )) }
     
+     </div>
      </div>
    </>
   )
