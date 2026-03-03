bcc8309de315e5dc2ace3ba45063e3422f7e1cbd
Amos Asiedu <asieduamos6@gmail.com>
2026-03-03 23:34:09 +0000

    text color and button bg changed in jobpage

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -20,18 +20,18 @@ const JobPage = () => {
    <>
     
     <div className=' bg-slate-100 px-8 '>
-      <h2 className='font-bold text-3xl text-center text-indigo-900 p-10 '>Browse Jobs</h2>
+      <h2 className='font-bold text-4xl text-center text-gray-900 p-10 mb-15 '>Browse Jobs</h2>
      <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
       {jobs.map((job) => (
      <div className=' px-5 pt-5 rounded-md shadow-lg bg-white'>
-        <h2 className='text-2xl text-gray-800 font-bold mb-5'>{job.title}</h2>
+        <h2 className='text-2xl text-gray-700 font-bold mb-5'>{job.title}</h2>
         <h4 className='font-thin  text-gray-500 mb-3'>{job.type}</h4>
         <p className='mb-3 text-gray-700 font-semibold'>{job.description}</p>
       <p className='font-thin text-gray-500 mb-4'>{job.salary}</p>
 
-        <div className='shadow-2xl py-5 px-4 rounded-lg mb-5'>
-          <h1 className='text-red-600 mb-3 ml-2 '>{job.location}</h1>
-          <button className='text-white px-30 py-3 py-2 mx-6 rounded-md bg-indigo-700 hover:bg-gray-700 
+        <div className='shadow-2xl py-5 px-4 mb-5 border-t border-gray-400'>
+          <h1 className='text-red-700 mb-3 ml-2 '>{job.location}</h1>
+          <button className='text-white px-30 py-3 py-2 mx-6 rounded-md bg-gray-900 hover:bg-gray-700 
            text-md font-semibold '>
             Apply</button>
         </div>
