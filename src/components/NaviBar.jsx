132a479acd5476550d23016816df0804637dea4c
Amos Asiedu <asieduamos6@gmail.com>
2026-03-03 23:18:19 +0000

    button bg color changed to red when it is active

--- src/components/NaviBar.jsx
+++ src/components/NaviBar.jsx
@@ -3,31 +3,32 @@ import FJ from '../images/FJ.png'
 import { NavLink } from 'react-router-dom'  
 
 const NaviBar = () => {
+
+  const activeStyle = ({ isActive }) => 
+    isActive ? 
+      'bg-red-700 text-white lg:font-semibold font-light lg:text-xl text-5xl px-5 lg:py-1 py-3 text-center rounded-md cursor-pointer hover:text-white transition duration-300 ease-in-out'
+     : 
+     'bg-black text-white lg:font-semibold font-light lg:text-xl text-5xl  px-5 lg:py-1 py-3 text-center rounded-md cursor-pointer hover:bg-gray-900 hover:text-whitetransition duration-300 ease-in-out';
+  
   return (
     <>
-    <div className="flex lg:h-20 md:h-50 h-60 lg:w-full w-6xl items-center border-t border-b border-indigo-600 shadow-lg">
-          <div className='flex'>
+    <div className="flex space-x-3 lg:h-20 md:h-50 h-60 lg:w-full w-6xl items-center border-t border-b border-indigo-600 shadow-lg">
+          <div className='flex mr-130'>
             <img className='pointer-events-none select-none lg:ml-30 md:ml-10 ml-15 lg:w-16 md:w-27 w-30' src={FJ} alt="" />
             <div className='font-extrabold italic lg:text-xl text-5xl lg:ml-1 ml-3 lg:mt-3 md:mt-6 mt-8 text-indigo-800'>
               Find Job</div> 
           </div>
             
-             
-             <NavLink to="/Home" className='bg-black  text-white lg:font-bold font-light lg:text-xl text-5xl lg:ml-100 md:ml-40 ml-30
-                       px-5 lg:py-1 py-3 text-center rounded-md cursor-pointer hover:bg-indigo-900 hover:text-white
-                       transition duration-300 ease-in-out'>
-                                Home
-                              </NavLink>
-             
-             <NavLink to="/addjob" className='bg-white border border-indigo-300 text-indigo-900 font-bold lg:text-xl text-5xl px-3
-                        lg:py-1 py-3 ml-5 mr-5 text-center rounded-md cursor-pointer hover:bg-indigo-900 hover:text-white 
-                        transition duration-300 ease-in-out'>
-                                Add Job
-                              </NavLink>
            
-             <NavLink to="/jobpage" className='bg-white border border-indigo-300 text-indigo-900 font-bold lg:text-xl text-5xl
-              px-6 lg:py-1 py-3 text-center rounded-md cursor-pointer hover:bg-indigo-900 hover:text-white 
-              transition duration-300 ease-in-out'>
+             <NavLink to="/Home" className= {activeStyle}  >
+               Home
+             </NavLink>
+             
+             <NavLink to="/addjob"className={activeStyle}   >
+                 Add Job
+             </NavLink>
+           
+             <NavLink to="/jobpage" className={activeStyle}  >
                   Jobs
              </NavLink>
             
