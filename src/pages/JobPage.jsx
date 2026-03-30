38a27c1b320c2b91b4025d36b181af08928bdd0c
Amos Asiedu <asieduamos6@gmail.com>
2026-03-30 13:42:03 +0000

    aply button changed to read more navlink in jobpage

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 
 
 
@@ -22,24 +23,25 @@ const JobPage = (job) => {
 
 return (
    <>
-    <div className=' bg-slate-100 px-8 '>
-      <h2 className='font-bold text-4xl text-center text-gray-900 p-10 mb-15 '>Browse Jobs</h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+    <div className=' bg-slate-100  '>
+      <h2 className='font-bold text-4xl text-center text-white p-10 my-5 bg-gray-600'>Browse Jobs</h2>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 px-8 mb-10'>
         {jobs.map((job) => (
             <div key={job.id} job={job} className=' px-5 pt-5 rounded-md shadow-lg bg-white' > 
                <h2 className='text-2xl text-gray-700 font-bold mb-5'>{job.title}</h2>
                <h4 className='font-thin  text-gray-500 mb-3'>{job.type}</h4>
                <p className='mb-3 text-gray-700 font-semibold'>{job.description.slice(0, 80)}...</p>
                <p className='font-thin text-gray-500 mb-4'>{job.salary}</p>
-                <div className='shadow-2xl py-5 px-4 mb-5 border-t border-gray-300'>
-                    <div className='text-red-700 mb-3 ml-1 '>
+
+               <div className='text-red-700 mb-3 ml-1 '>
                         <FaMapMarker className='inline text-sm mr-1' />
                       {job.location}
                     </div>
-                    <button className='text-white px-30 py-3 py-2 mx-6 rounded-md bg-gray-900 hover:bg-gray-600 
-                     text-md font-semibold '>
-                      Apply
-                    </button>
+                <div className='shadow-2xl py-5 mb-5 border-t border-gray-300'>
+                 <NavLink to={`/jobs/${job.id}`} className='text-white py-3 px-5 ml-53 rounded-md bg-gray-700 
+                 hover:bg-gray-600 text-md font-bold '>
+                      Read More
+                    </NavLink>
                 </div>
             </div>
        )) }
