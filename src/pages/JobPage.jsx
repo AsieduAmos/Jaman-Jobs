1ce8b9d7f624d1b106383df7a748249675489b23
Amos Asiedu <asieduamos6@gmail.com>
2026-03-30 12:58:29 +0000

    slicing job.description jobpage

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react'
 import { useState, useEffect } from 'react'
-import JobList from '../components/JobList';
+import { FaMapMarker } from 'react-icons/fa'
+
 
 
  
-
-const JobPage = () => {
+const JobPage = (job) => {
 
   const [jobs, setJobs] = useState([])
     
@@ -22,17 +22,30 @@ const JobPage = () => {
 
 return (
    <>
-    
     <div className=' bg-slate-100 px-8 '>
       <h2 className='font-bold text-4xl text-center text-gray-900 p-10 mb-15 '>Browse Jobs</h2>
-     <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
-      {jobs.map((job) => (
-        <JobList key={job.id} job={job} />  
-      )) }
-    
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+        {jobs.map((job) => (
+            <div key={job.id} job={job} className=' px-5 pt-5 rounded-md shadow-lg bg-white' > 
+               <h2 className='text-2xl text-gray-700 font-bold mb-5'>{job.title}</h2>
+               <h4 className='font-thin  text-gray-500 mb-3'>{job.type}</h4>
+               <p className='mb-3 text-gray-700 font-semibold'>{job.description.slice(0, 80)}...</p>
+               <p className='font-thin text-gray-500 mb-4'>{job.salary}</p>
+                <div className='shadow-2xl py-5 px-4 mb-5 border-t border-gray-300'>
+                    <div className='text-red-700 mb-3 ml-1 '>
+                        <FaMapMarker className='inline text-sm mr-1' />
+                      {job.location}
+                    </div>
+                    <button className='text-white px-30 py-3 py-2 mx-6 rounded-md bg-gray-900 hover:bg-gray-600 
+                     text-md font-semibold '>
+                      Apply
+                    </button>
+                </div>
+            </div>
+       )) }
+      </div>
      </div>
-     </div>
-   </>
+  </>
   )
 }
 
