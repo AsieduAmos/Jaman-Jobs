4379d8d6b5720b021cc56460573d9ddc9a8940ee
Amos Asiedu <asieduamos6@gmail.com>
2026-09-18 23:39:32 +0000

    some sentences reconstructed in the home page

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is a specialized job board platform designed to connect local talent with nearby opportunities. 
 The platform allows job seekers to find employment tailored to their profession specifically within their municipality or district. 
-Concurrently, companies and employers can list job vacancies to scout and hire the perfect local employees.
+Concurrently, companies and employers can list job vacancies to scout and hire the perfect local employees. 
+The platform includes a Job Posting page designed for employers to publish and track active job vacancies.
+To list a vacancy, employers can navigate to the Add Job page to post new openings.
+The platform includes an internal job board page where employees can browse, filter, and view open positions
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   <> 
   
      <div className='lg:text-xl md:text-5xl text-7xl lg:font-small md:font-light font-light 
-               bg-gray-800 text-white lg:py-16 py-15 text-center lg:mt-6 md:mt-12 mt-18 lg:w-full w-6xl px-20'>
+               bg-gray-400 text-white lg:py-10 py-15 text-center lg:mt-6 md:mt-12 mt-18 lg:w-full w-6xl px-20'>
         <p>Your Gateway to Career Growth and Top Talent.
           Whether you are looking for your next exciting job opportunity 
           or searching for the perfect employee, our platform bridges the gap. 
@@ -19,7 +19,7 @@ const HomePage = () => {
       </div>
 
         <div className='grid lg:grid-cols-2 grid-cols-1 items-center
-              pointer-events-none select-none lg:mt-10 mt-15 mb-10 ml-30'>
+              pointer-events-none select-none lg:mt-10 mt-15 mb-15 ml-30'>
            <div className='pr-30'>
               <p className='text-xl font-small text-gray-800'>
                 Find the right employee quickly and easily with our user-friendly platform.
@@ -28,17 +28,17 @@ const HomePage = () => {
               </p>
            </div>
            <img className='lg:ml-10 ml-100  w-100' src={employer} 
-             alt=" black young man looking on mobile phone" />
+             alt=" black man sitting beside a laptop on a table." />
        </div>
        <div className='bg-gray-200 text-gray-900 lg:w-3xl md:w-5xl w-5xl lg:py-5 py-20 lg:px-5 px-15 lg:ml-70 md:ml-90 ml-180
-         lg:mt-20 shadow-md lg:rounded-lg rounded-4xl'>
+         lg:mt-10 shadow-md lg:rounded-lg rounded-4xl'>
         <p className='lg:font-normal font-light  lg:text-base md:text-4xl text-6xl lg:pb-2 pb-15 lg:my-3 my-6'> 
           Our platform makes it easy to find the perfect fit for your team.
-           Post your job vacancies today and connect with top-tier talent tailored to your business needs.
+           Post your job vacancies in a minute  and connect with top-tier talent tailored to your business needs.
           
         </p>
         <NavLink to="/Addjob" className='bg-indigo-700 text-white lg:font-normal font-medium lg:text-lg md:text-4xl text-6xl 
-           lg:px-4 px-15 lg:py-1 md:py-4 py-5 text-center rounded-lg cursor-pointer hover:bg-black
+           lg:px-10 px-15 lg:py-1 md:py-4 py-5 ml-70 text-center rounded-lg cursor-pointer hover:bg-black
            hover:text-white transition duration-300 ease-in-out'>Add Job</NavLink>
       </div>
        
@@ -47,9 +47,7 @@ const HomePage = () => {
            <img className='ml-100 lg:ml-50 w-100' src={findjob} 
              alt=" black young man looking on mobile phone" />
            <div className='pr-30 '>
-             <h2 className='lg:font-bold font-extrabold lg:text-3xl md:text-5xl text-6xl mb-8  '>
-                Stay Home And Find Job
-             </h2>
+            
                <p className='text-xl font-small text-gray-800'>
                 Find your next job with a single click. 
                 Skip the stress and let us match you with opportunities
@@ -57,31 +55,17 @@ const HomePage = () => {
            </div>
        </div>
 
-  <div className='flex lg:flex-row flex-col items-center justify-center lg:mx-10 lg:gap-6 gap-10  '>
-    <div className='bg-gray-200 text-gray-900 lg:w-3xl md:w-5xl w-5xl lg:py-5 py-20 lg:px-5 px-15 lg:ml-1 md:ml-90 ml-180 lg:mt-10 shadow-md lg:rounded-lg rounded-4xl'>
-        <h2 className='lg:font-bold font-extrabold 
-         lg:text-2xl md:text-5xl text-7xl py-2 '>For Employers</h2>
-        <p className='lg:font-normal font-light  lg:text-base md:text-4xl text-6xl lg:pb-2 pb-15 lg:my-3 my-6'> 
-          Our platform makes it easy to find the perfect fit for your team.
-           Post your job vacancies today and connect with top-tier talent tailored to your business needs.
-          
-        </p>
-        <NavLink to="/Addjob" className='bg-indigo-700 text-white lg:font-normal font-medium lg:text-lg md:text-4xl text-6xl 
-           lg:px-4 px-15 lg:py-1 md:py-4 py-5 text-center rounded-lg cursor-pointer hover:bg-black
-           hover:text-white transition duration-300 ease-in-out'>Add Job</NavLink>
-      </div>
-
-     <div className='bg-gray-100 text-gray-900 lg:w-3xl md:w-5xl w-5xl lg:py-5 py-20 lg:px-5 px-15 lg:ml-1 md:ml-90 ml-180 mt-10 shadow-md lg:rounded-lg rounded-4xl'>
-        <h2 className='lg:font-bold font-semibold lg:text-2xl md:text-5xl text-7xl py-2 '>For Job Seekers</h2>
+     <div className='bg-gray-200 text-gray-900 lg:w-3xl md:w-5xl w-5xl lg:py-5 py-20 
+           lg:px-5 px-15 lg:ml-70 md:ml-90 ml-180 mt-10 shadow-md lg:rounded-lg rounded-4xl'>
         <p className='lg:font-normal font-light lg:text-base md:text-4xl text-6xl lg:pb-2 pb-15 lg:my-3 my-6'> 
            Unlock your professional potential by aligning your core strengths and
             personal ambitions with roles where you can truly thrive.
         </p>
-        <NavLink to="/jobpage" className='bg-black text-white lg:font-normal font-medium lg:text-lg md:text-4xl text-6xl 
-        lg:px-4 px-15 lg:py-1 md:py-4 py-5 text-center rounded-lg cursor-pointer hover:bg-indigo-700 
+        <NavLink to="/jobpage" className='bg-black ml-70 text-white lg:font-normal font-medium lg:text-lg md:text-4xl text-6xl 
+        lg:px-12 px-15 lg:py-1 md:py-4 py-5 text-center rounded-lg cursor-pointer hover:bg-indigo-700 
         hover:text-white transition duration-300 ease-in-out'>Find Job</NavLink>
-      </div>
-     </div> 
+     </div>
+      
     
   </>
   )
